884c1f08cbf2f3133677230aa925eeb7e77c2757
jcksnfales <jcksnfales@gmail.com>
2023-05-24 18:54:56 -0400

    Added book catalog page

--- app/site/routes.py
+++ app/site/routes.py
@@ -1,12 +1,19 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, request, redirect, flash
 from flask_login import current_user
-from models import Book, book_schema, books_schema
+from models import Book, book_schema, books_schema, db
+from forms import BookContributionForm
 
 site = Blueprint('site', __name__, template_folder='site_templates')
 
 @site.route('/')
 def landing():
     return render_template('index.html')
+    
+@site.route('/books')
+def book_catalog():
+    # query database for books
+    queried_books = Book.query.all()
+    return render_template('catalog.html', books=queried_books, books_json=books_schema.dump(queried_books))
 
 @site.route('/profile')
 def profile():
@@ -15,5 +22,6 @@ def profile():
         # if user is logged in, direct them to their profile
         return render_template('profile.html')
     else:
-        # otherwise, redirect them to the landing page
-        return redirect('/')
+        # otherwise, flash a warning and redirect them to the signin page
+        flash('You must be logged in to access the profile page.', category='access-contribute-failed')
+        return redirect('/signin')
