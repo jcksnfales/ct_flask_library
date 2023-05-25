37d8094880551901a62e5195446f200b2e255103
jcksnfales <jcksnfales@gmail.com>
2023-05-24 21:22:25 -0400

    Created template for book list
    + Added book list to profile page

--- app/site/routes.py
+++ app/site/routes.py
@@ -11,8 +11,9 @@ def landing():
     
 @site.route('/books')
 def book_catalog():
-    # query database for books
+    # query database for all books
     queried_books = Book.query.all()
+    # then direct user to catalog, passing in found books
     return render_template('catalog.html', books=queried_books, books_json=books_schema.dump(queried_books))
 
 @site.route('/contribute', methods=['GET','POST'])
@@ -43,16 +44,18 @@ def contribute():
         return render_template('contribute.html', form=form)
     else:
         # otherwise, flash a warning and redirect them to the signin page
-        flash('You must be logged in to contribute books.', category='access-profile-failed')
+        flash('You must be logged in to contribute books.', category='access-contribute-failed')
         return redirect('/signin')
-    
+
 @site.route('/profile')
 def profile():
     # check if there is a user currently logged in
     if current_user.is_authenticated:
-        # if user is logged in, direct them to their profile
-        return render_template('profile.html')
+        # if user is logged in, query database for books contributed by this user
+        queried_books = Book.query.filter_by(contributor_token=current_user.token).all()
+        # then direct user to profile, passing in found books
+        return render_template('profile.html', books=queried_books, books_json=books_schema.dump(queried_books))
     else:
         # otherwise, flash a warning and redirect them to the signin page
-        flash('You must be logged in to access the profile page.', category='access-contribute-failed')
+        flash('You must be logged in to access the profile page.', category='access-profile-failed')
         return redirect('/signin')
